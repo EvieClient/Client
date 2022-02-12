64ff4166c98921f3a7b7a6e9ae89931fee2c9c0b
Tristan Camejo <twisttaan@icloud.com>
2022-02-12 21:58:36 +1100

    fix: no more billy

--- src/main/java/com/evieclient/modules/impl/util/AccountsModule.java
+++ src/main/java/com/evieclient/modules/impl/util/AccountsModule.java
@@ -10,13 +10,4 @@ public class AccountsModule extends Module {
     public AccountsModule() {
         super("Accounts", "Changes the current logged-in account.", Category.UTIL, true);
     }
-
-    @Override
-    public void setupModule() {
-        try {
-            SessionChanger.getInstance().setUserOffline("billy", "c98187a9-e17b-3f48-8a33-882e62d18d19");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 }
